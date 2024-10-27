3424ee23cd8d675b4c42df59df7a8f19c085c256
Shawn Louis <shawnlouis2000@gmail.com>
2024-10-27 17:53:56 -0400

    Define message model

--- models.go
+++ models.go
@@ -1,1 +1,11 @@
 package main
+
+import "time"
+
+type Message struct {
+	ID           string    `json:"id"`
+	Text         string    `json:"text"`
+	IsPalindrome bool      `json:"isPalindrome"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
+}
