6ea62c876e333098d5a58fccb3791b8de80ba60d
Shawn Louis <shawnlouis2000@gmail.com>
2024-10-27 17:49:28 -0400

    Set up database connection

--- db_connection.go
+++ db_connection.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB(dataSourceName string) {
+	var err error
+	DB, err = sql.Open("postgres", dataSourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
