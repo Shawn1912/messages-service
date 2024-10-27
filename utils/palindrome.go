b0fbe9e305d8d9fb9df9d57b0fd8338c5be4c2e8
Shawn Louis <shawnlouis2000@gmail.com>
2024-10-27 18:14:35 -0400

    Add palindrome utility function

--- utils/palindrome.go
+++ utils/palindrome.go
@@ -1,1 +1,14 @@
 package utils
+
+import "strings"
+
+func IsPalindrome(s string) bool {
+	sanitized := strings.ToLower(strings.ReplaceAll(s, " ", ""))
+	length := len(sanitized)
+	for i := 0; i < length/2; i++ {
+		if sanitized[i] != sanitized[length-1-i] {
+			return false
+		}
+	}
+	return true
+}
