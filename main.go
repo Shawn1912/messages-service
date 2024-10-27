ce58925e476065afbb6242a3cb1799c5ce37d3bf
Shawn Louis <shawnlouis2000@gmail.com>
2024-10-27 19:06:05 -0400

    Set up the router and routes

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	InitDB("user=postgres dbname=messages sslmode=disable")
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/messages", CreateMessage).Methods("POST")
+	router.HandleFunc("/messages/{id:[0-9]+}", GetMessage).Methods("GET")
+	router.HandleFunc("/messages/{id:[0-9]+}", UpdateMessage).Methods("PUT")
+	router.HandleFunc("/messages/{id:[0-9]+}", DeleteMessage).Methods("DELETE")
+	router.HandleFunc("/messages", ListMessages).Methods("GET")
+
+	log.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", router)
+}
