8bd58999e981e824c3355b47fd3e94021851fb12
Shawn Louis <shawnlouis2000@gmail.com>
2024-10-27 23:51:12 -0400

    Fix failed test cases with punctuation in a palindrome

--- utils/palindrome.go
+++ utils/palindrome.go
@@ -1,12 +1,22 @@
 package utils
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
+// IsPalindrome checks if a given string is a palindrome.
 func IsPalindrome(s string) bool {
-	sanitized := strings.ToLower(strings.ReplaceAll(s, " ", ""))
-	length := len(sanitized)
+	var cleaned strings.Builder
+	for _, r := range s {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			cleaned.WriteRune(unicode.ToLower(r))
+		}
+	}
+	cleanedStr := cleaned.String()
+	length := len(cleanedStr)
 	for i := 0; i < length/2; i++ {
-		if sanitized[i] != sanitized[length-1-i] {
+		if cleanedStr[i] != cleanedStr[length-1-i] {
 			return false
 		}
 	}
